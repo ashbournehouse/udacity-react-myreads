73b144bc3c86e5c4c09eaf0fb06213b346ab8d8a
Andrew Biller <a.biller@ashho.net>
2022-04-06 13:24:45 +0100

    Move state-setting code from componentWillMount to constructor

--- src/Components/ShelfChanger/shelfchanger.jsx
+++ src/Components/ShelfChanger/shelfchanger.jsx
@@ -9,7 +9,11 @@ class ShelfChanger extends React.Component {
     shelvesData: [],
   }
 
-  componentWillMount() {
+  constructor(props) {
+    super(props);
+      //
+      // Update the state from props before rendering
+      //
     const {shelfName, shelves} = this.props;
     const tempShelvesData = this.state.shelvesData.slice(0);
     console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
@@ -37,6 +41,35 @@ class ShelfChanger extends React.Component {
     console.log('vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv');
   }
 
+/*
+  componentWillMount() {
+    const {shelfName, shelves} = this.props;
+    const tempShelvesData = this.state.shelvesData.slice(0);
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+    console.log(`Shelf name is: ${shelfName}`);
+    console.log(`Shelf options are:`);
+    shelves.map(shelf => {
+      console.log(` - ${shelf}`);
+    })
+    console.log('-----------------------------------');
+    console.log(JSON.stringify(tempShelvesData));
+    console.log('-----------------------------------');
+    shelves.map(shelf => {
+      console.log(`About to add to state for ${shelf}`);
+      tempShelvesData.push({
+            shelfName: shelf,
+            shelfValue: (shelf.toLowerCase().replace(/\s/g,'')),
+            shelfSelected: (shelf === shelfName ? true : false),
+      });
+    console.log('Updated shelvesData is:');
+    console.log(JSON.stringify(tempShelvesData));
+    });
+    this.state.shelvesData = tempShelvesData;
+    console.log('Updated state is:');
+    console.log(JSON.stringify(this.state));
+    console.log('vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv');
+  }
+*/
 
   handleSelect() {
     console.log(`Selected value is: ${event.target.value}`)
